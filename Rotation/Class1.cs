b48b8fe0bb4425475840d213323bde3efc2ad12d
Francis <super_f_qc@hotmail.com>
2015-09-11 13:15:05 -0400

    DLL Rotation V1

---
+++ Rotation/Class1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rotation
+{
+    public class Rotation
+    {
+        static String Chiffrer(String str, int Value)
+        {
+            String Encoder="";
+            foreach (Char lettre in str)
+            {
+                if(Char.IsDigit(lettre)) 
+                {
+                    Encoder += (Char)('0' + ((lettre + Value) % 10));
+                }
+                else if (Char.IsUpper(lettre))
+                {
+                    Encoder += (Char)('A' + ((lettre + Value) % 26));
+                }
+                else
+                {
+                    Encoder += (Char)('a' + ((lettre + Value) % 26));
+                }
+                
+            }
+            return Encoder;
+        }
+
+        static String Dechiffrer(String str, int Value)
+        {
+            String Encoder = "";
+            foreach (Char lettre in str)
+            {
+                if (Char.IsDigit(lettre))
+                {
+                    Encoder += (Char)('0' + ((lettre - Value) % 10));
+                }
+                else if (Char.IsUpper(lettre))
+                {
+                    Encoder += (Char)('A' + ((lettre - Value) % 26));
+                }
+                else
+                {
+                    Encoder += (Char)('a' + ((lettre - Value) % 26));
+                }
+
+            }
+            return Encoder;
+        }
+    }
+}
